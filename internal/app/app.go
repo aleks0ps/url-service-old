2a94361cd5ad564e42d618f67f0d822e4ed89d32
alex0ps <alexey.petrunev@yandex.ru>
2024-01-16 20:59:21 +0300

    refactor

--- internal/app/app.go
+++ internal/app/app.go
@@ -3,22 +3,14 @@ package app
 import (
 	"net/http"
 
+	"github.com/go-chi/chi/v5"
+
 	"github.com/aleks0ps/url-service/internal/app/handler"
 )
 
-func route(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case http.MethodPost:
-		handler.ShortenURL(w, r)
-	case http.MethodGet:
-		handler.GetOrigURL(w, r)
-	default:
-		w.WriteHeader(http.StatusBadRequest)
-	}
-}
-
 func Run() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", route)
-	http.ListenAndServe("localhost:8080", mux)
+	r := chi.NewRouter()
+	r.Get("/{id}", handler.GetOrigURL)
+	r.Post("/", handler.ShortenURL)
+	http.ListenAndServe("localhost:8080", r)
 }
--- internal/app/handler/handler_test.go
+++ internal/app/handler/handler_test.go
@@ -3,8 +3,9 @@ package handler
 import (
 	"net/http"
 	"net/http/httptest"
-	"strings"
 	"testing"
+
+	"github.com/go-resty/resty/v2"
 
 	"github.com/aleks0ps/url-service/internal/app/storage"
 
@@ -23,31 +24,41 @@ func TestShortenURL(t *testing.T) {
 		{method: http.MethodPost, body: "", expectedCode: http.StatusBadRequest, expectedBody: ""},
 	}
 
+	handler := http.HandlerFunc(ShortenURL)
+	srv := httptest.NewServer(handler)
+	defer srv.Close()
+
 	for _, tc := range testCases {
 		t.Run(tc.method, func(t *testing.T) {
-			r := httptest.NewRequest(tc.method, "/", strings.NewReader(tc.body))
-			r.Header.Set("Content-Type", contentType)
-			w := httptest.NewRecorder()
-			ShortenURL(w, r)
-			assert.Equal(t, tc.expectedCode, w.Code, "Код ответа не совпадает с ожидаемым")
+			r := resty.New().R()
+			r.Method = tc.method
+			r.URL = srv.URL
+			r.SetHeader("Content-Type", contentType)
+			r.SetBody([]byte(tc.body))
+			resp, err := r.Send()
+			assert.NoError(t, err, "error making HTTP request")
+			assert.Equal(t, tc.expectedCode, resp.StatusCode(), "Код ответа не совпадает с ожидаемым")
 		})
 	}
 }
 
 func TestGetOrigURL(t *testing.T) {
 	contentType := "text/plain"
-	baseUri := "http://localhost:8080/"
 	urls := []struct {
 		key     string
 		origUrl string
 	}{
-		{key: GenerateShortKey(), origUrl: "https://ya.ru"},
-		{key: GenerateShortKey(), origUrl: "https://google.com"},
+		{key: "qsBVYP", origUrl: "https://ya.ru"},
+		{key: "35D0WW", origUrl: "https://google.com"},
 	}
 
 	for _, url := range urls {
 		storage.StoreURL(url.key, url.origUrl)
 	}
+
+	handler := http.HandlerFunc(GetOrigURL)
+	srv := httptest.NewServer(handler)
+	defer srv.Close()
 
 	testCases := []struct {
 		method       string
@@ -55,17 +66,21 @@ func TestGetOrigURL(t *testing.T) {
 		expectedCode int
 		expectedBody string
 	}{
-		{method: http.MethodGet, body: baseUri + urls[0].key, expectedCode: http.StatusTemporaryRedirect, expectedBody: urls[0].origUrl},
-		{method: http.MethodGet, body: baseUri + urls[1].key, expectedCode: http.StatusTemporaryRedirect, expectedBody: urls[1].origUrl},
+		{method: http.MethodGet, body: urls[0].key, expectedCode: http.StatusTemporaryRedirect, expectedBody: urls[0].origUrl},
+		{method: http.MethodGet, body: urls[1].key, expectedCode: http.StatusTemporaryRedirect, expectedBody: urls[1].origUrl},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.method, func(t *testing.T) {
-			r := httptest.NewRequest(tc.method, tc.body, nil)
-			r.Header.Set("Content-Type", contentType)
-			w := httptest.NewRecorder()
-			GetOrigURL(w, r)
-			assert.Equal(t, tc.expectedCode, w.Code, "Код ответа не совпадает с ожидаемым")
+			r := resty.New().R()
+			r.Method = tc.method
+			r.URL = srv.URL + "/" + tc.body
+			r.SetHeader("Content-Type", contentType)
+			resp, err := r.Send()
+			assert.NoError(t, err, "error making HTTP request")
+			// return 200 instead of 30*
+			assert.Equal(t, http.StatusOK, resp.StatusCode(), "Код ответа не совпадает с ожидаемым")
+
 		})
 	}
 }
